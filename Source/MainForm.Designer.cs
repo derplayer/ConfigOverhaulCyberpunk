cdaa56f87d016f934121fca9f534bc41ea1128d5
derplayer <derplayer@outlook.com>
2020-12-14 21:14:27 +0100

    * Initial Commit

---
+++ Source/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ConfigOverhaulCyberpunk
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.veryLowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optimizedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disableAsyncComputeNVIDIAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.AllowUserToAddRows = false;
+            this.dataGridViewMain.AllowUserToDeleteRows = false;
+            this.dataGridViewMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewMain.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMain.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.Location = new System.Drawing.Point(12, 27);
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewMain.Size = new System.Drawing.Size(1354, 652);
+            this.dataGridViewMain.TabIndex = 0;
+            this.dataGridViewMain.DataMemberChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            this.dataGridViewMain.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValueChanged);
+            this.dataGridViewMain.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValueChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.veryLowToolStripMenuItem,
+            this.lowToolStripMenuItem,
+            this.optimizedToolStripMenuItem,
+            this.disableAsyncComputeNVIDIAToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1378, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // veryLowToolStripMenuItem
+            // 
+            this.veryLowToolStripMenuItem.Name = "veryLowToolStripMenuItem";
+            this.veryLowToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.veryLowToolStripMenuItem.Text = "Very Low";
+            this.veryLowToolStripMenuItem.Click += new System.EventHandler(this.VeryLowToolStripMenuItem_Click);
+            // 
+            // lowToolStripMenuItem
+            // 
+            this.lowToolStripMenuItem.Name = "lowToolStripMenuItem";
+            this.lowToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.lowToolStripMenuItem.Text = "Low";
+            this.lowToolStripMenuItem.Click += new System.EventHandler(this.lowToolStripMenuItem_Click);
+            // 
+            // optimizedToolStripMenuItem
+            // 
+            this.optimizedToolStripMenuItem.Name = "optimizedToolStripMenuItem";
+            this.optimizedToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.optimizedToolStripMenuItem.Text = "Optimized";
+            this.optimizedToolStripMenuItem.Click += new System.EventHandler(this.optimizedToolStripMenuItem_Click);
+            // 
+            // disableAsyncComputeNVIDIAToolStripMenuItem
+            // 
+            this.disableAsyncComputeNVIDIAToolStripMenuItem.Name = "disableAsyncComputeNVIDIAToolStripMenuItem";
+            this.disableAsyncComputeNVIDIAToolStripMenuItem.Size = new System.Drawing.Size(171, 20);
+            this.disableAsyncComputeNVIDIAToolStripMenuItem.Text = "Disable Async Compute only";
+            this.disableAsyncComputeNVIDIAToolStripMenuItem.Click += new System.EventHandler(this.disableAsyncComputeNVIDIAToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1378, 691);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Config Overhaul for Cyberpunk 2077 v1.0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem veryLowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optimizedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disableAsyncComputeNVIDIAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        public System.Windows.Forms.DataGridView dataGridViewMain;
+    }
+}
+
---
+++ Source/MainForm.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Runtime.InteropServices;
+
+using IniParser;
+using IniParser.Model;
+
+namespace ConfigOverhaulCyberpunk
+{
+    public partial class MainForm : Form
+    {
+        BindingList<REDEngineFeature> featureList;
+        string path = @".\engine\config\platform\pc\user.ini";
+        FileIniDataParser parser = new FileIniDataParser();
+        IniData redConfig;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            bool pathOk = VerifyPath();
+
+            if(pathOk == false)
+            {
+                OpenFileDialog choofdlog = new OpenFileDialog();
+                choofdlog.Filter = "REDprelauncher (REDprelauncher.exe)|REDprelauncher.exe";
+                choofdlog.FilterIndex = 1;
+                choofdlog.Multiselect = false;
+
+                if (choofdlog.ShowDialog() == DialogResult.OK)
+                {
+                    string sFileName = Path.GetDirectoryName(choofdlog.FileName);
+                    path = sFileName + @"\engine\config\platform\pc\user.ini";
+                }
+            }
+
+            this.Load += new EventHandler(Form1_Load);
+        }
+
+        public bool VerifyPath()
+        {
+            bool checkEngineDir = Directory.Exists(@".\engine");
+            if (checkEngineDir) return true;
+
+            bool checkDefaultSteamDir = Directory.Exists(@"C:\Program Files (x86)\Steam\steamapps\common\Cyberpunk 2077\");
+            if (checkDefaultSteamDir)
+            {
+                path = @"C:\Program Files (x86)\Steam\steamapps\common\Cyberpunk 2077\engine\config\platform\pc\user.ini";
+                return true;
+            }
+
+            return false;
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            //Verify path integrity
+            this.Load += new EventHandler(Form1_Load);
+
+            InitializeFeatureList();
+            dataGridViewMain.DataSource = featureList;
+            dataGridViewMain.Columns["Enabled"].Width = 50;
+            dataGridViewMain.Columns["GroupName"].ReadOnly = true;
+            dataGridViewMain.Columns["GroupName"].Width = 200;
+            dataGridViewMain.Columns["FunctionName"].ReadOnly = true;
+            dataGridViewMain.Columns["FunctionName"].Width = 200;
+            dataGridViewMain.Columns["Description"].ReadOnly = false;
+
+            dataGridViewMain.ClearSelection();
+        }
+
+        private void dataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //foreach (DataGridViewRow row in dataGridViewMain.SelectedRows)
+            //{
+            //    REDEngineFeature current = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[row.Index];
+
+            //    if (current.Enabled == true)
+            //        current.Enabled = false;
+            //    else
+            //        current.Enabled = true;
+            //}
+
+            dataGridViewMain.Refresh();
+            dataGridViewMain.ClearSelection();
+        }
+
+        private void InitializeFeatureList()
+        {
+            if (File.Exists(path))
+            {
+                //Load already generated INI
+                redConfig = parser.ReadFile(path);
+            }
+            else
+            {
+                //Create base file with all features enabled
+                redConfig = new IniData();
+                var featureDefinitions = LoadREDFeatures();
+
+                foreach (var featureDef in featureDefinitions)
+                {
+                    var kd = new KeyData(featureDef.FunctionName);
+                    kd.Value = "true";
+                    if (featureDef.Description != "") kd.Comments.Add(featureDef.Description);
+
+                    if (featureDef.GroupName.Contains("Developer"))
+                    {
+                        redConfig.Sections.AddSection(featureDef.GroupName);
+                        redConfig[featureDef.GroupName].AddKey(kd);
+                    }
+
+                    if (featureDef.GroupName.Contains("Rendering"))
+                    {
+                        redConfig.Sections.AddSection(featureDef.GroupName);
+                        redConfig[featureDef.GroupName].AddKey(kd);
+                    }
+
+                    if (featureDef.GroupName.Contains("RayTracing"))
+                    {
+                        redConfig.Sections.AddSection(featureDef.GroupName);
+                        redConfig[featureDef.GroupName].AddKey(kd);
+                    }
+                }
+
+                File.WriteAllText(path, redConfig.ToString());
+            }
+
+            featureList = new BindingList<REDEngineFeature>();
+
+            //Initialize DataGrid
+            foreach (var section in redConfig.Sections)
+            {
+                foreach (var featureSet in section.Keys)
+                {
+                    var featureTmp = new REDEngineFeature();
+                    featureTmp.Enabled = Convert.ToBoolean(featureSet.Value);
+                    featureTmp.GroupName = section.SectionName;
+                    featureTmp.FunctionName = featureSet.KeyName;
+                    if (featureSet.Comments.Count >= 1)
+                        featureTmp.Description = featureSet.Comments[0];
+                    featureList.Add(featureTmp);
+                }
+            }
+        }
+
+        public List<REDEngineFeature> LoadREDFeatures()
+        {
+            List<REDEngineFeature> list = new List<REDEngineFeature>();
+            List<string> lines = Properties.Resources.FeatureList.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            ).ToList();
+
+            foreach (var line in lines)
+            {
+                try
+                {
+                    string[] fields = line.Split(';');
+                    if (fields.Count() >= 3)
+                    {
+                        var feature = new REDEngineFeature
+                        {
+                            Enabled = true,
+                            GroupName = fields[0],
+                            FunctionName = fields[1],
+                            Description = fields[2]
+                        };
+
+                        list.Add(feature);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Something is wrong with a feature definition: " + e.ToString());
+                }
+            }
+
+            return list;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IniData redConfigNew = new IniData();
+            int disableCount = 0;
+            //datagrid to ini
+            foreach (DataGridViewRow row in dataGridViewMain.Rows)
+            {
+                REDEngineFeature featureDef = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[row.Index];
+
+                var kd = new KeyData(featureDef.FunctionName);
+                kd.Value = featureDef.Enabled.ToString();
+
+                if (featureDef.Enabled == false) disableCount++;
+
+                if (featureDef.Description != "") kd.Comments.Add(featureDef.Description);
+
+                if (featureDef.GroupName.Contains("Developer"))
+                {
+                    redConfigNew.Sections.AddSection(featureDef.GroupName);
+                    redConfigNew[featureDef.GroupName].AddKey(kd);
+                }
+
+                if (featureDef.GroupName.Contains("Rendering"))
+                {
+                    redConfigNew.Sections.AddSection(featureDef.GroupName);
+                    redConfigNew[featureDef.GroupName].AddKey(kd);
+                }
+
+                if (featureDef.GroupName.Contains("RayTracing"))
+                {
+                    redConfig.Sections.AddSection(featureDef.GroupName);
+                    redConfig[featureDef.GroupName].AddKey(kd);
+                }
+            }
+
+            //when everything is enabled, just clear the user.ini
+            if(disableCount >= 1)
+                File.WriteAllText(path, redConfigNew.ToString());
+            else
+                File.Delete(path);
+        }
+
+        public class REDEngineFeature
+        {
+            public bool Enabled { get; set; }
+            public string GroupName { get; set; }
+            public string FunctionName { get; set; }
+            public string Description { get; set; }
+        }
+
+        private void VeryLowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (var row in dataGridViewMain.Rows)
+            {
+                REDEngineFeature current = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[i];
+
+                current.Enabled = false;
+                if (current.FunctionName.Contains("DistantShadows")) current.Enabled = true;
+                if (current.FunctionName.Contains("GlobalIllumination")) current.Enabled = true;
+                i++;
+            }
+            dataGridViewMain.Refresh();
+        }
+
+        private void lowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (var row in dataGridViewMain.Rows)
+            {
+                REDEngineFeature current = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[i];
+                current.Enabled = false;
+                if (current.FunctionName.Contains("DistantShadows")) current.Enabled = true;
+                if (current.FunctionName.Contains("GlobalIllumination")) current.Enabled = true;
+                if (current.FunctionName.Contains("Hair")) current.Enabled = true;
+                if (current.FunctionName.Contains("RainMap")) current.Enabled = true;
+                if (current.FunctionName.Contains("Weather")) current.Enabled = true;
+                if (current.FunctionName.Contains("DynamicDecals")) current.Enabled = true;
+                if (current.FunctionName.Contains("UseSkinningLOD")) current.Enabled = true;
+
+                //more demanding options
+                if (current.FunctionName.Contains("SSAO") && current.GroupName.Contains("Feature")) current.Enabled = true;
+                if (current.FunctionName.Contains("Antialiasing")) current.Enabled = true;
+
+                i++;
+            }
+            dataGridViewMain.Refresh();
+        }
+
+        private void optimizedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (var row in dataGridViewMain.Rows)
+            {
+                REDEngineFeature current = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[i];
+                current.Enabled = true;
+
+                if (current.FunctionName.Contains("Character")) current.Enabled = false;
+                if (current.FunctionName.Contains("DistantFog")) current.Enabled = false;
+                if (current.FunctionName.Contains("DistantGI")) current.Enabled = false;
+                if (current.FunctionName.Contains("DistantVolFog")) current.Enabled = false;
+                if (current.FunctionName.Contains("MotionBlur")) current.Enabled = false;
+                if (current.FunctionName.Contains("Runtime")) current.Enabled = false;
+                if (current.FunctionName.Contains("ScreenSpace")) current.Enabled = false;
+                if (current.FunctionName.Contains("VolumetricClouds")) current.Enabled = false;
+                if (current.FunctionName.Contains("VolumetricFog")) current.Enabled = false;
+                if (current.FunctionName.Contains("UseExperimentalVolFog")) current.Enabled = false;
+                i++;
+            }
+            disableAsyncComputeNVIDIAToolStripMenuItem_Click(null, null);
+            dataGridViewMain.Refresh();
+        }
+
+        private void disableAsyncComputeNVIDIAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (var row in dataGridViewMain.Rows)
+            {
+                REDEngineFeature current = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[i];
+                if(current.GroupName.Contains("Async")) current.Enabled = false;
+                i++;
+            }
+            dataGridViewMain.Refresh();
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (var row in dataGridViewMain.Rows)
+            {
+                REDEngineFeature current = ((BindingList<REDEngineFeature>)dataGridViewMain.DataSource)[i];
+                current.Enabled = true;
+                i++;
+            }
+            dataGridViewMain.Refresh();
+        }
+    }
+}
